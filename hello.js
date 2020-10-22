58ac09cdd8e0c5eb5f28db259f9f6aedefbdf94b
Lev Michael <lev.choubine@gmail.com>
2020-10-22 10:03:00 -0400

    [js] first function 5 times

---
+++ hello.js
@@ -0,0 +1,34 @@
+function sayHello1(name1) {
+    let msg1 = 'Hello, ' + name1 + '. How are you?';
+    return msg1;
+}
+
+console.log(sayHello1('SEI 1019'))// feel free to change the string being processed
+
+function sayHello2(name2) {
+    let msg2 = 'Please to be a part of ' + name2 + '. How is everyone doing?!!';
+    return msg2 ;
+}
+
+console.log(sayHello2('RIGHT HERE')) // feel free to change the string being processed
+
+function sayHello3(name3) {
+    let msg3 = "What is for " + name3 + " today??!!";
+    return msg3;
+}
+
+console.log(sayHello3('Lunch'))// feel free to change the string being processed
+
+function sayHello4(name4) {
+    let msg4 = "Do you know what " + name4 + " it is??";
+    return msg4;
+}
+
+console.log(sayHello4("TIME")) // feel free to change the string being processed
+
+function sayHello5(name5) {
+    let msg5 = "Is it going to "  + name5 + " today??!!";
+    return msg5;
+}
+
+console.log(sayHello5('RAIN'))// feel free to change the string being processed
