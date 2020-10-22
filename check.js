fe5f5ab05a520008ea4559f3e7670ebaebad181c
Lev Michael <lev.choubine@gmail.com>
2020-10-22 10:15:46 -0400

    [js] wrote the first function 10 times

---
+++ check.js
@@ -0,0 +1,10 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    }else if (num <0){return 'negative';
+}else {
+    return 'zero';
+}
+}
+
+console.log(checkNumber(5)); //feel free to change the number
--- hello.js
+++ hello.js
@@ -32,3 +32,36 @@ function sayHello5(name5) {
 }
 
 console.log(sayHello5('RAIN'))// feel free to change the string being processed
+
+function sayHello6(name6) {
+    let msg6 = "This is a lot of " + name6 + "!!!!"; 
+    return msg6;
+}
+
+console.log(sayHello6('CODING'))// feel free to change the string being processed
+
+function sayHello7(name7){
+    let msg7= "It is " + name7 + " AM right now.";
+    return msg7;
+}
+
+console.log(sayHello7('10;00')); //feel free to change the string being processed
+
+function sayHello8(name8){
+    let msg8= " I would like so more " + name8 + " right now!!" ;
+    return msg8;
+}
+
+console.log(sayHello8('COFFEE!!')); //feel free to change the string being processed
+
+function sayHello9(name9){
+    let msg9 = "I am almost done with the " + name9 + " function!!";
+    return msg9;
+};
+console.log(sayHello9('FIRST')); //feel free to change the string being processed
+
+function sayHello10(name10){
+    let msg10 = "I wrote this funtcion " + name10 + " times";
+    return msg10;
+}
+console.log(sayHello10('10')); // feel free to change the string being processed
